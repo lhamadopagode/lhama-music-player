a88bcdf4a95b1b5a24bd61999e7a7f1b26742d2f
David Marquardt <davidmarquardt@me.com>
2018-01-23 22:17:10 -0300

    now player wrapper uses native event api from navigator

--- src/js/jukebox/components/soundCloudPlayerWrapper.js
+++ src/js/jukebox/components/soundCloudPlayerWrapper.js
@@ -1,13 +1,40 @@
+export const SoundCloudPlayerWrapperEvents = {
+  PLAYING: 'playing',
+  PAUSED: 'paused',
+  FINISHED: 'finished',
+};
+
 class SoundCloudPlayerWrapper {
   constructor(musicURI){
     this.id = musicURI;
     this.player = {};
+    this.event = {}
+    this.event[SoundCloudPlayerWrapperEvents.PLAYING] = new CustomEvent('playing',{
+      musicID: this.id,
+    });
+    this.event[SoundCloudPlayerWrapperEvents.PAUSED] = new CustomEvent('paused',{
+      musicID: this.id,
+    });
+    this.event[SoundCloudPlayerWrapperEvents.FINISHED] = new CustomEvent('finished',{
+      musicID: this.id,
+    });
+    this.event.buffered = new CustomEvent('buffered',{
+      musicID: this.id,
+    });
+
   }
 
   play(){
-    this.player = SC.stream('tracks/' + this.id).then((player) => {
-      debugger;
-      player.play();
+     SC.stream('tracks/' + this.id).then((player) => {
+      // debugger;
+      this.player = player;
+      this.player.play().then(_=>{
+        this.player.on('finish', _ => { 
+          window.dispatchEvent(this.event[SoundCloudPlayerWrapperEvents.FINISHED]) 
+        });
+      });
+      window.dispatchEvent(this.event[SoundCloudPlayerWrapperEvents.PLAYING]);
+      
     });
   }
   
@@ -15,5 +42,6 @@ class SoundCloudPlayerWrapper {
     this.player.pause();
   }
 }
+
 
 export default SoundCloudPlayerWrapper

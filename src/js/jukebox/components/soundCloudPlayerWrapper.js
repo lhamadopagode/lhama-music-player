458881b4aa0518ba0a8a5f1a048929c58754a50d
David Marquardt <davidmarquardt@me.com>
2018-01-27 21:17:45 -0300

    differ create a player object on sc sdk to play an existent one

--- src/js/jukebox/components/soundCloudPlayerWrapper.js
+++ src/js/jukebox/components/soundCloudPlayerWrapper.js
@@ -26,21 +26,40 @@ class SoundCloudPlayerWrapper {
   }
 
   play(){
-     SC.stream('tracks/' + this.id).then((player) => {
-      // debugger;
-      this.player = player;
-      this.player.play().then(_=>{
-        this.player.on('finish', _ => { 
-          window.dispatchEvent(this.event[SoundCloudPlayerWrapperEvents.FINISHED]) 
+    if(_.isUndefined(this.player)){
+      SC.stream('tracks/' + this.id).then((player) => {
+        // debugger;
+        this.player = player;
+        this.player.play().then(_=>{
+          this.player.on('finish', _ => { 
+            window.dispatchEvent(this.event[SoundCloudPlayerWrapperEvents.FINISHED]) 
+          });
         });
-      });
-      window.dispatchEvent(this.event[SoundCloudPlayerWrapperEvents.PLAYING]);
+        window.dispatchEvent(this.event[SoundCloudPlayerWrapperEvents.PLAYING]);
       
-    });
+      });
+    }
+    else {
+      this.player.play().then(_ => window.dispatchEvent(this.event[SoundCloudPlayerWrapperEvents.PLAYING]));
+    }
   }
   
   pause(){
     this.player.pause();
+  }
+
+  isPaused(){
+    if(this.player.getState() === 'paused'){
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  kill(){
+    this.player.kill();
+    window.dispatchEvent(this.event[SoundCloudPlayerWrapperEvents.FINISHED]);
   }
 }
 

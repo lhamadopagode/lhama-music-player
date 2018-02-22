b1673a07c50cc7085942386cc914089fbb27ef61
David Marquardt <davidmarquardt@me.com>
2018-02-22 11:28:07 -0300

    now play button has your own component

---
+++ src/js/jukebox/components/playButton.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faPlay from '@fortawesome/fontawesome-free-solid/faPlay';
+import faPause from '@fortawesome/fontawesome-free-solid/faPause';
+import faCircleNotch from '@fortawesome/fontawesome-free-solid/faCircleNotch';
+
+export default function PlayButton(props) {
+  if (props.tracks.lenght) {
+    if (props.isPlaying) {
+      return (
+        <button
+          className="jukebox__play-pause"
+          onClick={this.pause.bind(this)}
+        >
+          <FontAwesomeIcon
+            icon={faPause}
+            className={props.isPlaying ? '' : 'jukebox__play-pause-icon'}
+          />
+        </button>);
+    }
+    return (
+      <button
+        className="jukebox__play-pause"
+        onClick={props.playSong.bind(this)}
+      >
+        <FontAwesomeIcon
+          icon={faPlay}
+          className="jukebox__play-pause-icon"
+        />
+      </button>);
+  }
+  return (
+    <button
+      disabled
+      className="jukebox__play-pause"
+    >
+      <FontAwesomeIcon icon={faCircleNotch} />
+  </button>);
+}

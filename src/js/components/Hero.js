47d09e880a1d42e095cad28cefc83cb923335446
David Marquardt <davidmarquardt@me.com>
2018-02-22 11:27:39 -0300

    setting up hero for page

---
+++ src/js/components/Hero.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default function Hero(props) {
+  return (
+    <div className="header-hero">
+      <div className="wrapper">
+        <div className="header-hero__text-wrapper">
+          <h1 className="header-hero__title">Bem vindo ao seu próximo serviço de streaming.</h1>
+        </div>
+      </div>
+    </div>
+  );
+}

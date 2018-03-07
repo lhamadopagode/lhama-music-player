38494268719cf5e001113f16f807014aade4605c
David Marquardt <davidmarquardt@me.com>
2018-03-07 13:25:40 -0300

    playlist card component created

--- src/js/components/Hero.js
+++ src/js/components/Hero.js
@@ -7,6 +7,17 @@ export default function Hero(props) {
         <div className="header-hero__text-wrapper">
           <h1 className="header-hero__title">O limite da música é o infinito.</h1>
         </div>
+        <div className="header-hero__cards">
+        <h2 className="header-hero__cards-title">
+          Playlists
+        </h2>
+          <div className="playlist-card-wide__wrapper">
+            <a href="javascript:;" className="playlist-card-wide">
+              <h2 className="playlist-card-wide__title">Playlist Title</h2>
+              <img className="playlist-card-wide__cover" src="http://placehold.it/500x300" alt=""/>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
